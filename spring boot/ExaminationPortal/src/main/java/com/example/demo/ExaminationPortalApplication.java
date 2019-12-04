f7f72b77e57d27bf605011741095bdf924a742c5
wakeupabhi24 <46643731+wakeupabhi24@users.noreply.github.com>
2019-12-04 20:51:01 +0530

    first commit

---
+++ spring boot/ExaminationPortal/src/main/java/com/example/demo/ExaminationPortalApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExaminationPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExaminationPortalApplication.class, args);
+	}
+}
